e643cbb9abfe84c2eba133e7cd9dfec0f171ea20
Nick Messing <nicolai@richpanel.com>
2024-12-04 10:44:27 +0200

    DJ Section

---
+++ src/components/landing/DjHobbySection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="dj-hobby" class="dj-hobby-section">
+    <iframe
+      frameborder="0"
+      height="400"
+      src="https://player-widget.mixcloud.com/widget/iframe/?feed=%2FNiDee%2Fnick-messing-horizons-3%2F"
+      width="90%"
+    />
+  </div>
+</template>
+
+<style scoped>
+.dj-hobby-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--color-electric-blue);
+  height: var(--height-section);
+
+  iframe {
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.5);
+    border: 0;
+    border-radius: 1rem;
+  }
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AboutMeSection from '@/components/landing/AboutMeSection.vue'
+import DjHobbySection from '@/components/landing/DjHobbySection.vue'
 import HeroSection from '@/components/landing/HeroSection.vue'
 </script>
 
 <template>
   <HeroSection />
   <AboutMeSection />
+  <DjHobbySection />
 </template>
